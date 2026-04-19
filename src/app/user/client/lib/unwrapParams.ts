d7f7aec728b221d24b94fd6cbc6ba4bdc34ec3db
David Anderson <danderson.github@gmail.com>
2026-04-19 13:10:02 +0100

    removed unused user account pages

--- src/app/user/client/lib/unwrapParams.ts
+++
@@ -1,7 +0,0 @@
-import { use } from "react";
-
-export function unwrapParams<T>(params: T | Promise<T>) {
-  return typeof (params as Promise<T>)?.then === "function"
-    ? use(params as Promise<T>)
-    : (params as T);
-}
